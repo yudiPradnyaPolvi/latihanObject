8c058dbeaaa19ea1968812b5af9fadca40d577aa
yudiPradnyaPolvi <pradnyakemenuh@gmail.com>
2023-03-09 23:10:46 +0800

    first commit

---
+++ object.js
@@ -0,0 +1,50 @@
+const makanan = {
+    Bali : "Betutu",
+    jakarta : "bubur",
+    makasar : "coto",
+    madura : "sate",
+    jogja :"gudeg"
+
+}
+console.log (makanan.Bali)
+console.log (makanan.jakarta)
+console.log (makanan.makasar)
+console.log (makanan.madura)
+console.log (makanan.jogja)
+
+
+const objectWisata = {
+    Bali : "Gwk",
+    jakarta : "monas",
+    bandung: "kebun teh",
+    Medan : "danau Toba",
+    jogja :"malioboro"
+
+}
+console.log (objectWisata.Bali)
+console.log (objectWisata.jakarta)
+console.log (objectWisata.bandung)
+console.log (objectWisata.Medan)
+console.log (objectWisata.jogja)
+
+
+const namaPanitia= {
+    elektro : "rizal",
+    kesmas : "adelia",
+    ti : "audrey",
+    mesin : "deni",
+    arsi :"ayu"
+
+}
+const key1 = "elektro"
+const key2 = "kesmas"
+const key3 = "ti"
+const key4 = "mesin"
+const key5 = "arsi"
+
+console.log (namaPanitia [key1] )
+console.log (namaPanitia [key2] )
+console.log (namaPanitia [key3] )
+console.log (namaPanitia [key4] )
+console.log (namaPanitia [key5] )
+
